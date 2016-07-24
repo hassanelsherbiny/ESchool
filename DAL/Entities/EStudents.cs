4d2eed539dcf6dea25d24d981d45d90cb3fd8950
hassanelsherbiny <HassanElsherbiny55@gmail.com>
2016-07-24 08:22:02 +0200

    Add gallery functionality

---
+++ DAL/Entities/EStudents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class EStudents
+    {
+        public int ID { get; set; }
+        public string FNAME { get; set; }
+        public string LNAME { get; set; }
+        public string EMAIL { get; set; }
+        public string PASSWORD { get; set; }
+        public DateTime DOB { get; set; }
+        public string TELEPHONE { get; set; }
+        public string MOBILENO { get; set; }
+        public DateTime DOA { get; set; }
+        public string STATUS { get; set; }
+        public string GENDER { get; set; }
+        public int CID { get; set; }
+        public int SECID { get; set; }
+        public int GID { get; set; }
+    }
+}
