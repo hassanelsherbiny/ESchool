f553b95b46befc5ac6b0200c1ce4e2aa09a495d9
hassanelsherbiny <HassanElsherbiny55@gmail.com>
2017-01-18 11:48:02 +0200

    Implement mobile app integration

---
+++ Userlogin.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Userlogin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void SubmitBtn_Click(object sender, EventArgs e)
+    {
+        if (LSid.Text=="10812"&&LPass.Text=="admin")
+        {
+            Response.Redirect("./Dashboard.aspx");
+        }
+        LSid.Text = string.Empty;
+        LPass.Text = string.Empty;
+    }
+}
