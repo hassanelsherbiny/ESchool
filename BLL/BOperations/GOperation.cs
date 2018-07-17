cd05097d31711715822369f21f24080c883035c5
hassanelsherbiny <HassanElsherbiny55@gmail.com>
2018-07-17 06:23:02 +0200

    Create integration with language learning tools

---
+++ BLL/BOperations/GOperation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using DAL.Entities;
+using DAL.EOperations;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Data;
+using MySql.Data.MySqlClient;
+using MySql.Web;
+
+namespace BLL.BOperations
+{
+    public class GOperation
+    {
+        // Handle to the Guardian DBAccess class
+        OGuardian GuardianDb = null;
+
+        public GOperation()
+        {
+            GuardianDb = new OGuardian();
+        }
+
+        public DataSet GetGuardianByID(int id)
+        {
+            return GuardianDb.ViewGuardian(id);
+        }
+
+        public DataSet GetGuardianList()
+        {
+            return GuardianDb.ViewGuardianList();
+        }
+
+        public int UpdateGuardian(Guardian Guardian, int ID)
+        {
+            return GuardianDb.UpdateGuardian(Guardian, ID);
+        }
+        
+        public int AddNewGuardian(Guardian Guardian)
+        {
+            return GuardianDb.AddNewGuardian(Guardian);
+        }
+    }
+}
